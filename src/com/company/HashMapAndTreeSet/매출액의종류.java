a3b6e6a7857cee465231c86a2db94d152b21ab0d
kivv00ng <rldnd2637@naver.com>
2022-04-07 23:55:57 +0900

    HashMap And TwoPoninter

--- src/com/company/HashMapAndTreeSet/매출액의종류.java
+++ src/com/company/HashMapAndTreeSet/매출액의종류.java
@@ -1,22 +1,26 @@
 package com.company.HashMapAndTreeSet;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class 매출액의종류 {
-    public static int[] solution(int N, int M, int[] arr){
-        int[] answer= new int[N];
+    public static ArrayList<Integer> solution(int N, int K, int[] arr){
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> HM = new HashMap<>();
 
-        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int x: arr){
-            map.put(x, map.getOrDefault(x,0)+1);
-
+        for(int i =0; i<K-1; i++){
+            HM.put(arr[i], HM.getOrDefault(arr[i],0)+1);
         }
-        for (int x : map.keySet()){
-            System.out.println("x:"+x+" value:"+map.get(x));
-            
 
+        int lt = 0;
+        for (int rt=K-1;rt<N; rt++){
+            HM.put(arr[rt], HM.getOrDefault(arr[rt],0)+1);
+            answer.add(HM.size());
+            HM.put(arr[lt], HM.get(arr[lt])-1);
+            if(HM.get(arr[lt])==0) HM.remove(arr[lt]);
+            lt++;
         }
 
         return answer;
@@ -25,11 +29,11 @@ public class 매출액의종류 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int N = kb.nextInt();
-        int M = kb.nextInt();
+        int K = kb.nextInt();
         int[] arr = new int[N];
         for (int i = 0; i<N; i++){
             arr[i] = kb.nextInt();
         }
-        for(int x: solution(N,M, arr)) System.out.print(x+" ");
+        for(int x: solution(N,K, arr)) System.out.print(x+" ");
     }
 }
